39d2e3acfec67ff1542bc5cde8d57a675efa88c0
tzqrzx <tzqrzx@163.com>
2018-09-07 10:49:58 +0800

    测试

--- app/src/main/java/com/grandhyatt/snowbeer/view/activity/LoginActivity.java
+++ app/src/main/java/com/grandhyatt/snowbeer/view/activity/LoginActivity.java
@@ -96,7 +96,6 @@ public class LoginActivity extends ActivityBase implements IActivityBase,View.On
 
         //禁止滑动退出
         setSwipeBackEnable(false);
-       String a="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
         initView();
         bindEvent();
         refreshUI();
