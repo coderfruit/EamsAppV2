d08cc4935a6edd2083424f4eeff71dcc0dac425d
chunmiao3032 <yangchunmiao@crbln.com>
2018-09-16 13:51:55 +0800

    杨春苗20180916111222

--- app/src/main/java/com/grandhyatt/snowbeer/view/activity/LoginActivity.java
+++ app/src/main/java/com/grandhyatt/snowbeer/view/activity/LoginActivity.java
@@ -51,7 +51,7 @@ import butterknife.ButterKnife;
  * 登录窗体
  * 杨春苗
  * 20180907
- * 20180916 yangcm111
+ * 20180916 yangcm111222
  */
 public class LoginActivity extends ActivityBase implements IActivityBase,View.OnClickListener, TextView.OnEditorActionListener{
 
